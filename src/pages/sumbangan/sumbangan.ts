91e84a83236b2c046af2466330da729e8718f73b
unknown <ankarayu@gmail.com>
2017-10-28 11:05:35 +0700

    post donatur sesuai tujuan on progress

--- src/pages/sumbangan/sumbangan.ts
+++ src/pages/sumbangan/sumbangan.ts
@@ -1,9 +1,17 @@
 
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, App, ActionSheetController, LoadingController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { HistoryPage } from '../history/history';
+import {ListPage} from '../list/list';
+import { MyApp } from '../../app/app.component';
+import { storage } from 'firebase';
+import { Data } from '../../providers/data';
+import { Http } from '@angular/http';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { BarangProvider } from '../../providers/data_barang_yayasan';
+
 
 /**
  * Generated class for the YayasanPostPage page.
@@ -21,7 +29,11 @@ export class SumbanganPage {
   // @ViewChild('jenis_barang') jenis_barang;
   @ViewChild('berat_barang') berat_barang;
   @ViewChild('volume_barang') volume_barang;
-  @ViewChild('keterangan') keterangan;
+  @ViewChild('deskripsi') deskripsi;
+  @ViewChild('kondisi_barang') kondisi_barang;
+  @ViewChild('jumlah_barang') jumlah_barang;
+  image: string;
+  id_post: string;
   jenis_barang:string;
   penerima: string;
   item:any;
@@ -33,12 +45,19 @@ export class SumbanganPage {
               public navParams: NavParams, 
               public alerCtrl: AlertController,
               private fire:AngularFireAuth,
-              private firedata: AngularFireDatabase
+              private firedata: AngularFireDatabase,
+              public http: Http, 
+              public data: Data,
+              private camera: Camera,
+              public loadCtrl: LoadingController,
+              public actionSheetCtrl: ActionSheetController,
               ) {
-  	this.item = this.navParams.get('penerima');
-  	console.log(this.item);
-
-  }
+                {
+                  this.item = this.navParams.data;
+                  console.log(this.item.$key);
+                }
+              }
+  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad YayasanPostPage');
@@ -53,17 +72,70 @@ export class SumbanganPage {
   }
 
 
-  post(){
+  post_donatur(){
       var user = this.fire.auth.currentUser; 
-      this.firedata.list('/post_donatur/')
-        .push({penerima: this.item, user: user.uid,  nama_barang: this.nama_barang.value, jenis_barang:this.jenis_barang, volume_barang: this.volume_barang.value, keterangan: this.keterangan.value, status: 1});
+      this.firedata.list('/data_barang_donatur/')
+        .push(
+          {
+            penerima_yayasan: this.item, 
+            donatur: user.uid,  
+            nama_barang: this.nama_barang.value, 
+            jenis_barang:this.jenis_barang, 
+            kondisi_barang: this.kondisi_barang, 
+            jumlah_barang: this.jumlah_barang.value, 
+            deskripsi: this.deskripsi.value,
+            status:1
+          }).then(data => {
+              this.id_post = data.path.pieces_[1];
+            })
+  
+          const picture = storage().ref('picture/foto_barang_donatur/'+user.uid+'--'+this.id_post);
+          picture.putString(this.image, 'data_url');
+  
+          storage().ref().child('picture/foto_barang_donatur/'+user.uid+'--'+this.id_post).getDownloadURL().then(url =>{
+            // ini kedata base
+            this.firedata.object('/data_barang_donatur/'+ user.uid).update({
+            image: url })
+          })
+  
       console.log('got data', user);
-   
-/*      console.log(this.nama_barang.value);
-      console.log(this.volume_barang.value);
-      console.log(this.berat_barang.value);
-      console.log(this.keterangan.value);
-      console.log(this.jenis_barang)*/
+      this.navCtrl.setRoot(ListPage);
       this.doAlert();
+  }
+
+  uploadBarangDonatur() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Pilihan',
+      buttons: [
+        // {
+        //   text: 'Ambil Gambar Baru',
+        //   role: 'ambilGambar',
+        //   handler: () => {
+        //     //this.takePicture();
+        //   }
+        // },
+        {
+          text: 'Pilih Dari Galleri',
+          role: 'gallery',
+          handler: () => {
+            this.getPhotoFromGallery();
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  getPhotoFromGallery(){
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        sourceType     : this.camera.PictureSourceType.PHOTOLIBRARY,
+        targetWidth: 600,
+        targetHeight: 600
+    }).then((imageData) => {
+      
+      this.image = 'data:image/jpeg;base64,' + imageData;
+      })
+            
   }
 }
